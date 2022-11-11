3da44893a86b96c5a8aec9b31080b01f682aa5ac
gxnas <52808122+gxnas@users.noreply.github.com>
2022-11-11 08:41:33 +0800

    Update diy-part.sh

--- build/x64_wjq/diy-part.sh
+++ build/x64_wjq/diy-part.sh
@@ -96,7 +96,6 @@ feeds.buildinfo
 openwrt-x86-64-generic-kernel.bin
 openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz
-openwrt-x86-64-squashfs-combined-efi.img.gz
 sha256sums
 version.buildinfo
 ipk.tar.gz
