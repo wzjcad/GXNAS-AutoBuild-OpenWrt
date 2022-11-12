5f1e7972e2c577017154fe06e8e26d872fee6a7c
gxnas <52808122+gxnas@users.noreply.github.com>
2022-11-12 22:58:39 +0800

    Update diy-part.sh

--- build/x64_test/diy-part.sh
+++ build/x64_test/diy-part.sh
@@ -8,9 +8,9 @@
 
 
 cat >$NETIP <<-EOF
-uci set network.lan.ipaddr='172.16.10.1'                      # IPv4 地址(openwrt后台地址)
+uci set network.lan.ipaddr='192.168.1.11'                      # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                   # IPv4 子网掩码
-uci set network.lan.gateway='172.16.10.2'                     # 旁路由设置 IPv4 网关（去掉uci前面的#生效）
+uci set network.lan.gateway='192.168.1.1'                     # 旁路由设置 IPv4 网关（去掉uci前面的#生效）
 #uci set network.lan.broadcast='192.168.1.255'                # 旁路由设置 IPv4 广播（去掉uci前面的#生效）
 uci set network.lan.dns='223.5.5.5 114.114.114.114'           # 旁路由设置 DNS(多个DNS要用空格分开)（去掉uci前面的#生效）
 uci set network.lan.delegate='0'                              # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
